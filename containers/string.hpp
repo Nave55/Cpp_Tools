37044df1720f8389347e4eb58a1a7e5c1df34afd
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2026-09-16 15:55:24 -0600

    Refactor String class to use ptr instead of string

--- containers/string.hpp
+++ containers/string.hpp
@@ -17,7 +17,7 @@ private:
 public:
   size_t len = 0;
   size_t cap = 0;  // total bytes including space for '\0'
-  char* string{nullptr};
+  char* ptr{nullptr};
 
 public:
   // empty string with initial capacity (including room for '\0')
@@ -25,12 +25,12 @@ public:
       : m_alloc{&alloc},
         len{0},
         cap{cap},
-        string{static_cast<char*>(
+        ptr{static_cast<char*>(
             m_alloc->allocate(sizeof(char) * cap, alignof(char)))} {
     if (sizeof(char) * cap > m_alloc->getChunkSize()) {
       panic("Initial String capacity does not fit in a pool chunk");
     }
-    string[0] = '\0';
+    ptr[0] = '\0';
   }
 
   // from C-string
@@ -38,12 +38,12 @@ public:
       : m_alloc{&alloc},
         len{std::strlen(str)},
         cap{len + 1},
-        string{static_cast<char*>(
+        ptr{static_cast<char*>(
             m_alloc->allocate(sizeof(char) * cap, alignof(char)))} {
     if (sizeof(char) * cap > m_alloc->getChunkSize())
       panic("Initial String capacity does not fit in a pool chunk");
 
-    std::memcpy(string, str, len + 1);  // includes '\0'
+    std::memcpy(ptr, str, len + 1);  // includes '\0'
   }
 
   // from C-string
@@ -56,19 +56,19 @@ public:
 
     this->cap = cap;
 
-    string = static_cast<char*>(
+    ptr = static_cast<char*>(
         m_alloc->allocate(sizeof(char) * cap, alignof(char)));
 
-    if (!string) panic("String(cap): allocation failed");
+    if (!ptr) panic("String(cap): allocation failed");
 
     if (sizeof(char) * cap > m_alloc->getChunkSize())
       panic("Initial String capacity does not fit in a pool chunk");
 
-    std::memcpy(string, str, len + 1);  // includes '\0'
+    std::memcpy(ptr, str, len + 1);  // includes '\0'
   }
   ~String() {
-    if (string && m_alloc->getType() == AllocType::Pool) {
-      m_alloc->free(string);
+    if (ptr && m_alloc->getType() == AllocType::Pool) {
+      m_alloc->free(ptr);
     }
   }
 
@@ -76,17 +76,17 @@ public:
       : m_alloc{str.m_alloc},
         len{str.len},
         cap{str.cap},
-        string{static_cast<char*>(
+        ptr{static_cast<char*>(
             m_alloc->allocate(sizeof(char) * str.cap, alignof(char)))} {
-    std::memcpy(string, str.string, len + 1);  // copy including '\0'
+    std::memcpy(ptr, str.ptr, len + 1);  // copy including '\0'
   }
 
   String(String&& str)
       : m_alloc{str.m_alloc},
         len{str.len},
         cap{str.cap},
-        string{str.string} {
-    str.string = nullptr;
+        ptr{str.ptr} {
+    str.ptr = nullptr;
     str.len = 0;
     str.cap = 0;
   }
@@ -131,21 +131,21 @@ public:
   };
 
   char& operator[](size_t i) noexcept {
-    return string[i];
+    return ptr[i];
   }
 
   const char& operator[](size_t i) const noexcept {
-    return string[i];
+    return ptr[i];
   }
 
   std::optional<char&> at(size_t i) noexcept {
     if (i >= len) return std::nullopt;
-    return string[i];
+    return ptr[i];
   }
 
   std::optional<const char&> at(size_t i) const noexcept {
     if (i >= len) return std::nullopt;
-    return string[i];
+    return ptr[i];
   }
 
   String& operator+=(char c) noexcept {
@@ -161,22 +161,22 @@ public:
       m_resizeCapacity(new_cap);
     }
 
-    std::memcpy(string + len, str, n + 1);
+    std::memcpy(ptr + len, str, n + 1);
     len += n;
 
     return *this;
   }
 
   char* begin() const noexcept {
-    return string;
+    return ptr;
   }
 
   char* end() const noexcept {
-    return string + len;
+    return ptr + len;
   }
 
   void print() const noexcept {
-    std::printf("%s\n", string);
+    std::printf("%s\n", ptr);
   }
 
   void printInfo() const noexcept {
@@ -188,39 +188,39 @@ public:
   }
 
   char first() const noexcept {
-    return string[0];
+    return ptr[0];
   }
 
   char last() const noexcept {
-    return string[len - 1];
+    return ptr[len - 1];
   }
 
   std::optional<char> firstSafe() const noexcept {
     if (len == 0) return std::nullopt;
-    return string[len];
+    return ptr[len];
   }
 
   std::optional<char> lastSafe() const noexcept {
     if (len == 0) return std::nullopt;
-    return string[len - 1];
+    return ptr[len - 1];
   }
 
   std::string_view toStringView() const noexcept {
-    return std::string_view(string);
+    return std::string_view(ptr);
   }
 
   std::string_view slice(size_t start, size_t end) const noexcept {
-    return std::string_view(string).substr(start, (end - start) + 1);
+    return std::string_view(ptr).substr(start, (end - start) + 1);
   }
 
   void clear() noexcept {
     len = 0;
-    if (string) string[0] = '\0';
+    if (ptr) ptr[0] = '\0';
   }
 
   void rtrim() {
-    while (len > 0 && isspace((unsigned char)string[len - 1])) {
-      string[len - 1] = '\0';
+    while (len > 0 && isspace((unsigned char)ptr[len - 1])) {
+      ptr[len - 1] = '\0';
       len--;
     }
   }
@@ -228,19 +228,19 @@ public:
   void ltrim() {
     size_t i = 0;
 
-    while (i < len && isspace(static_cast<unsigned char>(string[i]))) i++;
+    while (i < len && isspace(static_cast<unsigned char>(ptr[i]))) i++;
 
     if (i == 0) return;
     if (i == len) {
-      string[0] = '\0';
+      ptr[0] = '\0';
       len = 0;
       return;
     }
 
-    memmove(string, string + i, len - i);
+    memmove(ptr, ptr + i, len - i);
 
     len -= i;
-    string[len] = '\0';
+    ptr[len] = '\0';
   }
 
   void trim() {
@@ -250,18 +250,18 @@ public:
 
   void toLower() noexcept {
     for (size_t i = 0; i < len; ++i) {
-      char tmp = string[i];
+      char tmp = ptr[i];
       if (tmp >= 65 && tmp <= 90) {
-        string[i] = tmp + 32;
+        ptr[i] = tmp + 32;
       }
     }
   }
 
   void toUpper() noexcept {
     for (size_t i = 0; i < len; ++i) {
-      char tmp = string[i];
+      char tmp = ptr[i];
       if (tmp >= 97 && tmp <= 122) {
-        string[i] = tmp - 32;
+        ptr[i] = tmp - 32;
       }
     }
   }
@@ -281,20 +281,19 @@ public:
   }
 
   void reverse() noexcept {
-    for (size_t i = 0; i < len / 2; i++)
-      std::swap(string[i], string[len - 1 - i]);
+    for (size_t i = 0; i < len / 2; i++) std::swap(ptr[i], ptr[len - 1 - i]);
   }
 
   bool containsChar(char x) const noexcept {
     for (size_t i = 0; i < len; i++) {
-      if (string[i] == x) return true;
+      if (ptr[i] == x) return true;
     }
     return false;
   }
 
   std::optional<size_t> findChar(char x) const noexcept {
     for (size_t i = 0; i < len; i++)
-      if (string[i] == x) return i;
+      if (ptr[i] == x) return i;
     return std::nullopt;
   }
 
@@ -303,7 +302,7 @@ public:
 
     for (size_t i = 0; i <= len - sub.len; ++i) {
       size_t j = 0;
-      while (j < sub.len && string[i + j] == sub[j]) ++j;
+      while (j < sub.len && ptr[i + j] == sub[j]) ++j;
       if (j == sub.len) return true;
     }
 
@@ -315,7 +314,7 @@ public:
 
     for (size_t i = 0; i <= len - sub.len; ++i) {
       size_t j = 0;
-      while (j < sub.len && string[i + j] == sub[j]) ++j;
+      while (j < sub.len && ptr[i + j] == sub[j]) ++j;
       if (j == sub.len) return std::optional<Pair>({i, i + j - 1});
     }
 
@@ -338,8 +337,8 @@ public:
     requires std::is_same_v<std::remove_cvref_t<S>, char>
   void pushBack(S&& val) noexcept {
     if (len + 1 >= cap) m_resizeCapacity(cap * 2);
-    string[len++] = std::forward<S>(val);
-    string[len] = '\0';
+    ptr[len++] = std::forward<S>(val);
+    ptr[len] = '\0';
   }
 
   template <typename S>
@@ -348,58 +347,58 @@ public:
     if (ind > len) ind = len;
     if (len + 1 >= cap) m_resizeCapacity(cap * 2);
 
-    for (size_t i = len + 1; i > ind; --i) string[i] = string[i - 1];
+    for (size_t i = len + 1; i > ind; --i) ptr[i] = ptr[i - 1];
 
-    string[ind] = std::forward<S>(val);
+    ptr[ind] = std::forward<S>(val);
     ++len;
-    string[len] = '\0';
+    ptr[len] = '\0';
   }
 
   void pop() noexcept {
     if (len > 0) {
       --len;
-      string[len] = '\0';
+      ptr[len] = '\0';
     }
   }
 
   std::optional<char> popBack() noexcept {
     if (len == 0) return std::nullopt;
-    char val = string[len - 1];
+    char val = ptr[len - 1];
     --len;
-    string[len] = '\0';
+    ptr[len] = '\0';
     return val;
   }
 
   char popBackUnsafe() noexcept {
-    char val = string[len - 1];
+    char val = ptr[len - 1];
     --len;
-    string[len] = '\0';
+    ptr[len] = '\0';
     return val;
   }
 
   void orderedRemove(size_t ind) noexcept {
     if (ind >= len) return;
-    for (size_t i = ind; i + 1 < len; ++i) string[i] = string[i + 1];
+    for (size_t i = ind; i + 1 < len; ++i) ptr[i] = ptr[i + 1];
     --len;
-    string[len] = '\0';
+    ptr[len] = '\0';
   }
 
   void unorderedRemove(size_t ind) noexcept {
     if (ind >= len) return;
-    string[ind] = string[len - 1];
+    ptr[ind] = ptr[len - 1];
     --len;
-    string[len] = '\0';
+    ptr[len] = '\0';
   }
 
   void deleteVal(char val) noexcept {
     for (int i = static_cast<int>(len) - 1; i >= 0; --i) {
-      if (string[i] == val) orderedRemove(static_cast<size_t>(i));
+      if (ptr[i] == val) orderedRemove(static_cast<size_t>(i));
     }
   }
 
   void deleteValUnordered(char val) noexcept {
     for (int i = static_cast<int>(len) - 1; i >= 0; --i) {
-      if (string[i] == val) unorderedRemove(static_cast<size_t>(i));
+      if (ptr[i] == val) unorderedRemove(static_cast<size_t>(i));
     }
   }
 
@@ -410,20 +409,20 @@ private:
 
     if (m_alloc->supportsResize()) {
       char* new_str = static_cast<char*>(
-          m_alloc->resize(string, old_bytes, new_bytes, alignof(char)));
+          m_alloc->resize(ptr, old_bytes, new_bytes, alignof(char)));
       if (!new_str) panic("Allocator resize failed");
 
-      string = new_str;
+      ptr = new_str;
       cap = new_cap;
     } else {
       char* new_str = static_cast<char*>(
           m_alloc->allocate(sizeof(char) * new_cap, alignof(char)));
       if (!new_str) panic("Allocator resize failed");
 
-      std::memcpy(new_str, string, len + 1);  // include '\0'
+      std::memcpy(new_str, ptr, len + 1);  // include '\0'
 
-      m_alloc->free(string);
-      string = new_str;
+      m_alloc->free(ptr);
+      ptr = new_str;
       cap = new_cap;
     }
   }
